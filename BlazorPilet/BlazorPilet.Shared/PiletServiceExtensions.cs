06ab184b71a484230667750ab69e640da6a27023
Jesse Gollub <jesse.gollub@ewe.net>
2023-11-17 10:15:01 +0100

    moved showNotification into shared project

---
+++ BlazorPilet/BlazorPilet.Shared/PiletServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Piral.Blazor.Utils;
+
+namespace BlazorPilet.Shared;
+
+/// <summary>
+/// Extends the functionality of <see cref="IPiletService"></see>
+/// </summary>
+public static class PiletServiceExtensions
+{
+    private record NotificationOptions(int AutoClose);
+
+    /// <param name="piletService"></param>
+    /// <param name="content"></param>
+    /// <param name="autoClose">Closes the notification after the specified milliseconds</param>
+    /// <returns></returns>
+    public static Task ShowNotification(this IPiletService piletService, string content, int autoClose)
+    {
+        return piletService.ShowNotification(content, new NotificationOptions(autoClose));
+    }
+}
